20d1dfa800f2e02e1090367205c5b022a176e9f7
Livni <llmc0383@gmail.com>
2023-05-22 08:08:36 -0600

    creación del mensaje cuando se guarde exitosamente un dato.

--- controladores/insumos/guardar.php
+++ controladores/insumos/guardar.php
@@ -13,3 +13,37 @@ try {
 } catch (Exception $e2){
     $error = $e2->getMessage();
 }
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Insumos</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if($resultado): ?>
+                    <div class="alert alert-success" role="alert">
+                        Guardado exitosamente!
+                    </div>
+                <?php else :?>
+                    <div class="alert alert-danger" role="alert">
+                        Ocurrió un error: <?= $error ?>
+                    </div>
+                <?php endif ?>
+              
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="/parcial_martinez_c/vistas/insumos/index.php" class="btn btn-info">Regresar al formulario</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
