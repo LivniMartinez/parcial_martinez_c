f8dabbc2ed24b20144dbd855a521647f0fd75221
Livni <llmc0383@gmail.com>
2023-05-22 08:57:28 -0600

    finalizacion de formulario

--- vistas/insumos/index.php
+++ vistas/insumos/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Examne_parcial</title>
+    <title>Examen_parcial</title>
     <style>
     body {
     text-align: justify;
@@ -17,7 +17,7 @@
 </head>
 <body>
     <div class="container">
-        <h1 class="text-center">Formulario de registro de insumos de la Oficina de Logistica</h1>
+        <h1 class="bg-dark text-white text-center p-2 w-100">Formulario de registro de insumos de la Oficina de Logistica</h1>
         <div class="row justify-content-center">
             <form action="/parcial_martinez_c/controladores/insumos/guardar.php" method = "POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
