a9c640a5ac33e40cb09065441ae1bd9832ba603d
Livni <llmc0383@gmail.com>
2023-05-22 08:17:12 -0600

    correccion tabla

--- vistas/insumos/tabla_insumos.sql
+++ vistas/insumos/tabla_insumos.sql
@@ -4,7 +4,7 @@ CREATE TABLE insumos(
     insu_cantidad INTEGER,
     insu_marca VARCHAR(70),
     insu_proveedor VARCHAR(70),
-    insu_precio DECIMAL (8,2),
+    insu_precio DECIMAL (8,2)
 );
 
 Select * from insumos
