4be2a84a2f8b8aacc681013a2569f0f95207ffb3
Livni <llmc0383@gmail.com>
2023-05-22 08:28:55 -0600

    ingreso de datos metodo __construct y funcion guardar

--- modelos/insumos.php
+++ modelos/insumos.php
@@ -14,4 +14,21 @@ class Insumos extends Conexion {
     public $insu_precio;
 
 }
+public function __construct ($argumentos = [])
+    {
+        $this->insu_id = $argumentos ['insu_id'] ?? null;
+        $this->insu_nombre = $argumentos ['insu_nombre'] ?? '';
+        $this->insu_cantidad = $argumentos ['insu_cantidad'] ?? '';
+        $this->insu_marca = $argumentos ['insu_marca'] ?? '';
+        $this->insu_proveedor = $argumentos ['insu_proveedor'] ?? '';
+        $this->insu_precio = $argumentos ['insu_precio'] ?? '';
+
+    }   
+
+    public function guardar (){
+        $sql = "INSERT INTO insumos(insu_nombre, insu_cantidad, insu_marca, insu_proveedor, insu_precio) 
+        values ('$this->insu_nombre', '$this->insu_cantidad', '$this->insu_marca', '$this->insu_proveedor', '$this->insu_precio')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }  
 <?
